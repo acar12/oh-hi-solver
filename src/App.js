112be737928b1a5e96625ffb37906268acd6cca8
acar12 <39702495+acar12@users.noreply.github.com>
2022-11-05 22:01:02 -0500

    basic layout + css :)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import "./App.css"
 import SizeSelector from "./SizeSelector"
 
 let App = () => {
+    const [size, setSize] = useState(4)
+    const [grid, setGrid] = useState([])
+
+    if (grid.length !== size) {
+        let temp = [];
+        for (let i = 0; i < size; i++) {
+            temp.push(Array.from(Array(size), () => <button className="cell-button" value="0" />))
+        }
+        setGrid(temp)
+    }
+
     return (
-        <div className="app">
+        <div id="app">
             <h1>0h h1 Solver</h1>
-            <SizeSelector />
+            <div id="table">
+                {grid.map((r, i) => <div key={i}>{r}</div>)}
+            </div>
+            <div id="inputs">
+                <SizeSelector size={size} setSize={setSize} />
+                <button id="solve-button">Solve</button>
+            </div>
         </div>
     );
 }
